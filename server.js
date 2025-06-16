f1afc693df0ed31a815ba5487235fda958f1ff63
Joao Victor dos anjos Sales <salesvictor@icloud.com>
2025-06-16 18:24:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/predict-btc')));
+// Atualize aqui com base no outputPath real
+const distPath = path.join(__dirname, 'dist/predict-btc');
+console.log("distPath",distPath)
+
+app.use(express.static(distPath));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/predict-btc/index.html'));
+  res.sendFile(path.join(distPath, '/index.html'));
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
