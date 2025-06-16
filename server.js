0097262b26dd8d1e40ed61fa0a0c95fc54728285
Joao Victor dos anjos Sales <salesvictor@icloud.com>
2025-06-16 18:20:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist'))); // ou 'dist/nome-do-app'
+app.use(express.static(path.join(__dirname, 'dist/predict-btc')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/predict-btc/index.html'));
 });
 
 const port = process.env.PORT || 3000;
